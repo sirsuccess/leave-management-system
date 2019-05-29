deba9569df3d44889d73561b9c23cba6ad1ecdf1
sirsuccess <kanuamani@gmail.com>
2019-05-29 19:55:28 +0100

    signup connected to backend

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,37 @@
 
 $(document).ready(function(){
 
-    var submit = $("#test")
-    console.log("am seeing you")
-    submit.click(function(){
+  //window.location.href = "login.html";
+
+    var submit = $("#submit");
+    
+
+    // fName.blur(function () {
+    //   console.log(fName.val())
+    // })
+    
+
+    submit.click(function(event){
+      // event.preventDefault();
+      console.log("am seeing you")
+      var fName = $("#fName").val();
+    var lName = $("#lName").val();
+    var username = $("#username").val();
+    var email = $("#email").val();
+    var password = $("#password").val()
+
+       console.log("list item"+[lName,username,fName,email,password])
       console.log("am inside function");
-    var url ="http://localhost:3000/roles"
-      // $.post("http://localhost:3000/roles",
-      // {
-      //   role: "Donald Duck",
-      // },
-      // function(data,status){
-      //   alert("Data: " + data + "\nStatus: " + status);
-      // });
-      $.post("http://localhost:3000/roles", {
-        role:'amani'
-      }, function(response){ 
-      alert("success");
+    var url ="http://localhost:3000/users";
+      $.post(url, {
+      "first-name": fName,
+      "last-name": lName,
+      "email": email,
+      "username": username,
+      "password": password,
+      "roleID": 1
+      }, function(data){ 
+      alert("success"+ data.fName);
       // $("#mypar").html(response.amount);
 });
     });
